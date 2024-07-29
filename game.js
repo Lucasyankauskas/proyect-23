036808f330a3570aeb5e6fe882f946c4b7c3a729
Lucasyankauskas <yankauskas_lucas1@hotmail.com>
2024-07-29 15:53:58 -0300

    primer commit

---
+++ game.js
@@ -0,0 +1,31 @@
+
+
+const config = {
+    type: Phaser.AUTO,
+    width: 256,
+    height: 244,
+    backgroundColor: '#fff',
+    parent: 'game',
+    scene: {
+        preload,
+        create,
+        update
+    }
+};
+
+new Phaser.Game(config);
+
+function preload() {
+
+};
+
+function create() {
+
+};
+
+
+
+
+function update() {
+
+};
